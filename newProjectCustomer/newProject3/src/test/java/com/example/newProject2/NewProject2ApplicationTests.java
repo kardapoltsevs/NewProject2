4d888b55b0040f88e36670f6ef08bb7f10eddedf
user <mr.sergei82@mail.ru>
2025-06-08 06:40:24 +0300

    1

---
+++ newProjectCustomer/newProject3/src/test/java/com/example/newProject2/NewProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newProject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
